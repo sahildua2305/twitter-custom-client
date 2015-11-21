d527ccaf94483ea2b1a65a0fdc7ebe13dc761e5b
sahildua2305 <sahildua2305@gmail.com>
2015-11-21 21:34:31 +0530

    Added basic index.php and .gitignore

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
-LIVE LIVE :D :D 
+<?php
+
+	// include Slim
+	require 'Slim/Slim.php';
+	\Slim\Slim::registerAutoloader();
+
+	// initiate Slim in DEBUG mode
+	$app = new \Slim\Slim(array(
+		'debug' => true,
+		'mode' => 'development',
+		'log.enables' => true,
+		'log.level' => \Slim\Log::DEBUG
+	));
+	$app->config('debug', true);
+
+	/** Routes */
+
+
+	/** Functions */
+
+
+?>
