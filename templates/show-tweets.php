fe53854ce36191e4e80dd4b5bf6c97767fa763b2
sahildua2305 <sahildua2305@gmail.com>
2015-11-25 01:21:26 +0530

    Updated show-tweets.php, debugged

--- templates/show-tweets.php
+++ templates/show-tweets.php
@@ -12,27 +12,31 @@
 	<div class="visibile-lg" style="margin: 5% 0;"></div>
 	<div class="container text-center">
 		<img class="image-head" src="img/3be2c501646a37b5de1f6381df25a4dd.png">
-		<div><p style="font-family: 'Poiret One', cursive;">Showing tweets for #custserv</p></div>
+		<div><p style="font-family: 'Poiret One', cursive;">Showing tweets for #custserv with atleast one retweet</p></div>
 		<div class="row">
 			<div class="col-md-8 col-md-offset-2">
 				<div id="instant_results">
 					<?php
 
-						// $tweets = $tweets->statuses;
+						if ( !$error ) {
+						
+							$tweets = $tweets->statuses;
 
-						// foreach($tweets as $tweet){
-						// 	if ( $tweet->retweet_count == 0 )
-						// 		continue;
-						// 	echo "<div class='timeline-tweets'>";
-						// 	echo "<img src='".$tweet->user->profile_image_url."' class='img-thumbnail timeline' width='50'>";
-						// 	echo "<p><a href='http://twitter.com/intent/user?screen_name=".$tweet->user->screen_name."' target='_blank'>".($tweet->user->name)." <span class='text-muted'>@".$tweet->user->screen_name."</span></a></p>";
-						// 	echo ($tweet->text)."<br>";
-						// 	echo "<span class='text-muted small'>".date("g:i: A D, F jS Y",strtotime($tweet->created_at))."</span>";
-						// 	echo "<p class='tweet-controls'>";
-						// 	echo "<a href='https://twitter.com/intent/tweet?in_reply_to=".$tweet->id_str."' target='_blank'> Reply</a>  |  <a href='https://twitter.com/intent/favorite?tweet_id=".$tweet->id_str."' target='_blank'>Favorite</a>  |  <a href='https://twitter.com/intent/retweet?tweet_id=".$tweet->id_str."' target='_blank'>Retweet</a>";
-						// 	echo "</p>";
-						// 	echo "</div>";
-						// }
+							foreach($tweets as $tweet){
+								if ( $tweet->retweet_count == 0 )
+									continue;
+								echo "<div class='timeline-tweets'>";
+								echo "<img src='".$tweet->user->profile_image_url."' class='img-thumbnail timeline' width='50'>";
+								echo "<p><a href='http://twitter.com/intent/user?screen_name=".$tweet->user->screen_name."' target='_blank'>".($tweet->user->name)." <span class='text-muted'>@".$tweet->user->screen_name."</span></a></p>";
+								echo ($tweet->text)."<br>";
+								echo "<span class='text-muted small'>".date("g:i: A D, F jS Y",strtotime($tweet->created_at))."</span>";
+								echo "<p class='tweet-controls'>";
+								echo "<a href='https://twitter.com/intent/tweet?in_reply_to=".$tweet->id_str."' target='_blank'> Reply</a>  |  <a href='https://twitter.com/intent/favorite?tweet_id=".$tweet->id_str."' target='_blank'>Favorite</a>  |  <a href='https://twitter.com/intent/retweet?tweet_id=".$tweet->id_str."' target='_blank'>Retweet</a>";
+								echo "</p>";
+								echo "</div>";
+							}
+							
+						}
 
 					?>
 				</div>
